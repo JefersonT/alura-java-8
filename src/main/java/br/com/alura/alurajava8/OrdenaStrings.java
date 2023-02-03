d1c54ed23244d980c021332c8299dbd195e2a5e1
Jeferson Torress <tqi_jtorres@uolinc.com>
2023-02-03 14:54:45 -0300

    AULA 01. Default Methods

---
+++ src/main/java/br/com/alura/alurajava8/OrdenaStrings.java
@@ -0,0 +1,57 @@
+package br.com.alura.alurajava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+
+		//ANTES DO JAVA 8
+		System.out.println("ANTES DO JAVA 8");
+
+			//ORDENAR COM COMPARADOR NATURAL
+		System.out.println("---ORDENAR COM COMPARADOR NATURAL");
+		Collections.sort(palavras);
+		System.out.println(palavras);
+
+			//ORDENAR COM COMPARADOR DEFINIDO
+		System.out.println("---ORDENAR COM COMPARADOR DEFINIDO");
+		Comparator<String> comparator = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparator);
+		System.out.println(palavras);
+
+			// IMPRIMIR CADA ELEMENTO DA LISTA
+		System.out.println("---IMPRIMIR CADA ELEMENTO DA LISTA");
+		for (String p : palavras) {
+			System.out.println(p);
+		}
+
+
+		//APARTIR DO JAVA 8
+		System.out.println("APARTIR DO JAVA 8");
+
+			// ORDENAR COM COMPADADOR DEFINIDO
+		System.out.println("---ORDENAR COM COMPADADOR DEFINIDO");
+		Comparator<String> comparator2 = new ComparadorPorTamanho();
+		palavras.sort(comparator2);
+		System.out.println(palavras);
+
+			// IMPRIMIR CADA ELEMENTO DA LISTA
+		System.out.println("---IMPRIMIR CADA ELEMENTO DA LISTA");
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+	}
+
+}
